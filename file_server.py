2376a960f844810b28e1b8e2db6d92620afbacff
Yue <YueBeh>
2024-10-01 11:10:10 +0930

    Trying to adjust flask server to work on other devices

--- file_server.py
+++ file_server.py
@@ -26,4 +26,4 @@ def download_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
